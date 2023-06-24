2f671d5dac71bb233bc2cf270d3d94a7314e098d
5HT <maxim@synrc.com>
2023-06-24 12:28:22 +0300

    ldif

---
+++ priv/ldif/0-setup.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# OpenLDAP tests
+
+ldapadd -x -D "cn=Manager,dc=synrc,dc=com" -w secret -c -f init.ldif
+ldapadd -x -D "cn=Manager,dc=synrc,dc=com" -w secret -c -f add.ldif
+ldapadd -x -D "cn=Manager,dc=synrc,dc=com" -w secret -c -f add-exists.ldif
+ldapadd -x -D "cn=Manager,dc=synrc,dc=com" -w secret -c -f modify-replace.ldif
+ldapadd -x -D "cn=Manager,dc=synrc,dc=com" -w secret -c -f modify-add.ldif
+ldapadd -x -D "cn=Manager,dc=synrc,dc=com" -w secret -c -f modify-del.ldif
+ldapadd -x -D "cn=Manager,dc=synrc,dc=com" -w secret -c -f modify-multi.ldif
+ldapadd -x -D "cn=Manager,dc=synrc,dc=com" -w secret -c -f modify-noobj.ldif
+ldapadd -x -D "cn=Manager,dc=synrc,dc=com" -w secret -c -f modify-dn.ldif
+ldapadd -x -D "cn=Manager,dc=synrc,dc=com" -w secret -c -f delete.ldif
+ldapadd -x -D "cn=Manager,dc=synrc,dc=com" -w secret -c -f delete-noobj.ldif
+
+ldapsearch -D "cn=Manager,dc=synrc,dc=com" -w secret -b "dc=synrc,dc=com"
+ldapsearch -D "cn=Manager,dc=synrc,dc=com" -w secret -b "ou=People,dc=synrc,dc=com" "(&(uid=*)(sn=Ton*))"
+ldapsearch -D "cn=Manager,dc=synrc,dc=com" -w secret -b "ou=People,dc=synrc,dc=com" "uid=*" "objectClass"
+ldapsearch -D "cn=Manager,dc=synrc,dc=com" -w secret -b "ou=People,dc=synrc,dc=com" "uid=*" "sn"
