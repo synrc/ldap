f090676c1679f856398f2f2396af906119c03658
5HT <maxim@synrc.com>
2023-06-25 11:50:24 +0300

    ldif cleanup, WSL, brew OpenLDAP coverage

--- priv/ldif/0-setup.sh
+++ priv/ldif/0-setup.sh
@@ -2,23 +2,21 @@
 
 # OpenLDAP tests
 
-ldapadd -x -D "cn=Manager,dc=synrc,dc=com" -w secret -c -f init.ldif
-ldapadd -x -D "cn=Manager,dc=synrc,dc=com" -w secret -c -f add.ldif
-ldapadd -x -D "cn=Manager,dc=synrc,dc=com" -w secret -c -f add-exists.ldif
+ldapadd -x -D "cn=admin,dc=synrc,dc=com" -w secret -c -f init.ldif 
+ldapadd -x -D "cn=admin,dc=synrc,dc=com" -w secret -c -f add.ldif 
+ldapadd -x -D "cn=admin,dc=synrc,dc=com" -w secret -c -f add-exists.ldif 
+ldapadd -x -D "cn=admin,dc=synrc,dc=com" -w secret -c -f modify-replace.ldif 
+ldapadd -x -D "cn=admin,dc=synrc,dc=com" -w secret -c -f modify-add.ldif 
+ldapadd -x -D "cn=admin,dc=synrc,dc=com" -w secret -c -f modify-del.ldif 
+ldapadd -x -D "cn=admin,dc=synrc,dc=com" -w secret -c -f modify-multi.ldif 
+ldapadd -x -D "cn=admin,dc=synrc,dc=com" -w secret -c -f modify-noobj.ldif 
+ldapadd -x -D "cn=admin,dc=synrc,dc=com" -w secret -c -f modify-dn.ldif 
+ldapadd -x -D "cn=admin,dc=synrc,dc=com" -w secret -c -f delete.ldif 
+ldapadd -x -D "cn=admin,dc=synrc,dc=com" -w secret -c -f delete-noobj.ldif 
 
-ldapadd -x -D "cn=Manager,dc=synrc,dc=com" -w secret -c -f modify-replace.ldif
-ldapadd -x -D "cn=Manager,dc=synrc,dc=com" -w secret -c -f modify-add.ldif
-ldapadd -x -D "cn=Manager,dc=synrc,dc=com" -w secret -c -f modify-del.ldif
-ldapadd -x -D "cn=Manager,dc=synrc,dc=com" -w secret -c -f modify-multi.ldif
-ldapadd -x -D "cn=Manager,dc=synrc,dc=com" -w secret -c -f modify-noobj.ldif
-ldapadd -x -D "cn=Manager,dc=synrc,dc=com" -w secret -c -f modify-dn.ldif
-
-ldapadd -x -D "cn=Manager,dc=synrc,dc=com" -w secret -c -f delete.ldif
-ldapadd -x -D "cn=Manager,dc=synrc,dc=com" -w secret -c -f delete-noobj.ldif
-
-ldapsearch -D "cn=admin,cn=config" -w secret -b "cn=config"
-ldapsearch -D "cn=Manager,dc=synrc,dc=com" -w secret -b "dc=synrc,dc=com"
-ldapsearch -D "cn=Manager,dc=synrc,dc=com" -w secret -b "ou=People,dc=synrc,dc=com" "(&(uid=*)(sn=Ton*))"
-ldapsearch -D "cn=Manager,dc=synrc,dc=com" -w secret -b "ou=People,dc=synrc,dc=com" "uid=*" "objectClass"
-ldapsearch -D "cn=Manager,dc=synrc,dc=com" -w secret -b "ou=People,dc=synrc,dc=com" '(sn=To*)' cn sn
-ldapcompare -D "uid=admin,dc=synrc,dc=com" -w secret uid=admin,dc=synrc,dc=com uid:admin
+ldapsearch -D "cn=admin,cn=config" -w secret -b "cn=config" -Y EXTERNAL -H ldapi:///
+ldapsearch -D "cn=admin,dc=synrc,dc=com" -w secret -b "dc=synrc,dc=com"  -Y EXTERNAL -H ldapi:///
+ldapsearch -D "cn=admin,dc=synrc,dc=com" -w secret -b "ou=People,dc=synrc,dc=com" -Y EXTERNAL -H ldapi:/// "(&(uid=*)(sn=Ton*))"
+ldapsearch -D "cn=admin,dc=synrc,dc=com" -w secret -b "ou=People,dc=synrc,dc=com" -Y EXTERNAL -H ldapi:/// "uid=*" "objectClass"
+ldapsearch -D "cn=admin,dc=synrc,dc=com" -w secret -b "ou=People,dc=synrc,dc=com" -Y EXTERNAL -H ldapi:/// '(sn=To*)' cn sn 
+ldapcompare -D "cn=admin,dc=synrc,dc=com" -w secret  -Y EXTERNAL -H ldapi:/// uid=admin,dc=synrc,dc=com uid:admin 
