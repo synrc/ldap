c55032ff3e81dd3d465a2c874785d61de3043d38
5HT <maxim@synrc.com>
2023-06-26 05:39:29 +0300

    slapd conf

--- lib/LDAP.ex
+++ lib/LDAP.ex
@@ -3,19 +3,17 @@ defmodule LDAP do
   def testConnection() do
 #      {:ok, conn} = :eldap.open(['127.0.0.1'], [{:port, 389}, {:ssl, false}])
       {:ok, conn} = :eldap.open(['127.0.0.1'], [{:port, 636}, {:ssl, true},
-                    {:sslopts, [{:verify,:verify_none},
-                                {:cacertfile,'/home/maxim/depot/chat/priv/mosquitto/caroot.pem'},
-                                {:certs_keys, %{certfile: '/home/maxim/depot/chat/priv/mosquitto/client.pem',
-                                                keyfile: '/home/maxim/depot/chat/priv/mosquitto/client.key'}}]}])
+                    {:sslopts, [{:verify,true},
+                                {:cacertfile,'/Users/maxim/depot/chat/priv/mosquitto/caroot.pem'},
+                                {:certs_keys, %{certfile: '/Users/maxim/depot/chat/priv/mosquitto/client.pem',
+                                                keyfile: '/Users/maxim/depot/chat/priv/mosquitto/client.key'}}]}])
       :ok = :eldap.simple_bind(conn, 'cn=admin,dc=synrc,dc=com', 'secret')
       filter = :eldap.and([ ])
       scope = :wholeSubtree
       base = 'dc=synrc,dc=com'
       attrs = ['cn','mail']
-      {:ok, {:eldap_search_result, entries, []}} =
-             :eldap.search(conn, base: base, scope: scope,
-                    filter: filter, attributes: attrs)
-
+      {:ok, x} = :eldap.search(conn, base: base, scope: scope, filter: filter, attributes: attrs)
+      entries = :erlang.element(2, x)
       :lists.map(fn {:eldap_entry, dn, attrs} -> {:dn,dn,attrs} end, entries)
   end
 
--- priv/suite/run.sh
+++ priv/suite/run.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
 sudo pkill slapd
-sudo netstat -tnlp | grep "389\|636"
-sudo slapd -h "ldap:/// ldapi:/// ldaps:///" -g openldap -u openldap -F /etc/ldap/slapd.d
-sudo netstat -tnlp | grep "389\|636"
+sudo ./slapd -h "ldap:/// ldapi:/// ldaps:///"
+ps aux | grep slapd
