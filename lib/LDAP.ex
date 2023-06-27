7faca8864f02ff53bcbce443bb74946ddff0dd44
5HT <maxim@synrc.com>
2023-06-27 17:09:12 +0300

    synrc.com ldif

--- lib/LDAP.ex
+++ lib/LDAP.ex
@@ -5,13 +5,13 @@ defmodule LDAP do
       {:ok, conn} = :eldap.open(['127.0.0.1'], [{:port, 636}, {:ssl, true}])
       :eldap.start_tls(conn, [])
       :ok = :eldap.simple_bind(conn, 'cn=admin,dc=synrc,dc=com', 'secret')
-      filter = :eldap.and([ ])
-      scope = :wholeSubtree
-      base = 'dc=synrc,dc=com'
-      attrs = ['cn','mail']
-      {:ok, x} = :eldap.search(conn, base: base, scope: scope, filter: filter, attributes: attrs)
-      entries = :erlang.element(2, x)
-      :lists.map(fn {:eldap_entry, dn, attrs} -> {:dn,dn,attrs} end, entries)
+      filter   = {:filter, :eldap.and([ ])}
+      scope    = {:scope, :wholeSubtree}
+      base     = {:base, 'dc=synrc,dc=com'}
+      attrs    = {:attributes, ['cn','mail']}
+      {:ok, x} = :eldap.search(conn, [base, scope, filter, attrs])
+      :lists.map(fn {:eldap_entry, dn, attrs} ->
+                    {:dn,dn,attrs} end, :erlang.element(2, x))
   end
 
 end
--- priv/suite/populate.sh
+++ priv/suite/populate.sh
@@ -15,3 +15,5 @@ ldapadd -x -D "cn=admin,dc=synrc,dc=com" -w secret -c -f delete.ldif
 ldapadd -x -D "cn=admin,dc=synrc,dc=com" -w secret -c -f delete-noobj.ldif
 
 ldapadd -x -D "cn=admin,cn=config" -w secret -c -f ssl-mac.ldif
+
+ldapadd -x -D "cn=admin,dc=synrc,dc=com" -w secret -c -f erp.uno.ldif
