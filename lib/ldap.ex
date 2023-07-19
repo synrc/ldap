3bc3c4e91983b26a2acb4d3716eb644ee80a36b6
5HT <maxim@synrc.com>
2023-07-19 08:17:10 +0300

    schema

--- lib/ldap.ex
+++ lib/ldap.ex
@@ -1,19 +1,28 @@
 defmodule LDAP.TCP do
    require LDAP
 
-   def start(), do: :erlang.spawn(fn -> listen(1489,:binary.encode_hex(:crypto.strong_rand_bytes(8))) end)
+   def code(), do: :binary.encode_hex(:crypto.strong_rand_bytes(8))
 
-   def insertLDAP(db, dn, attrs) do
+   def start() do
+       instance = code()
+       :io.format 'SYNRC LDAP Instance: ~p~n', [instance]
+       :erlang.spawn(fn -> listen(1489,instance) end)
    end
 
    def listen(port,path) do
        {:ok, conn} = Exqlite.Sqlite3.open(path)
-       Exqlite.Sqlite3.execute(conn, "create table ldap (rdn text, att text, val binary)")
+       Exqlite.Sqlite3.execute(conn,
+          "begin;" <>
+          "create table ldap (rdn text, att text, val binary);" <>
+          "create index ldap_rdn on ldap (rdn);" <>
+          "create index ldap_att on ldap (att);" <>
+          "create index ldap_val on ldap (val);" <>
+          "commit;")
        {:ok, statement} = Exqlite.Sqlite3.prepare(conn, "insert into ldap (rdn,att,val) values (?1,?2,?3)")
        :ok = Exqlite.Sqlite3.bind(conn, statement, ["cn=admin,dc=synrc,dc=com","cn","admin"])
        :done = Exqlite.Sqlite3.step(conn, statement)
        {:ok, statement} = Exqlite.Sqlite3.prepare(conn, "insert into ldap (rdn,att,val) values (?1,?2,?3)")
-       :ok = Exqlite.Sqlite3.bind(conn, statement, ["cn=admin,dc=synrc,dc=com","password","secret"])
+       :ok = Exqlite.Sqlite3.bind(conn, statement, ["cn=admin,dc=synrc,dc=com","rootpw","secret"])
        :done = Exqlite.Sqlite3.step(conn, statement)
        {:ok, socket} = :gen_tcp.listen(port,
          [:binary, {:packet, 0}, {:active, false}, {:reuseaddr, true}])
@@ -26,44 +35,42 @@ defmodule LDAP.TCP do
        accept(socket,conn)
    end
 
-   def message(no, socket, {:abandonRequest, _}, db) do
-       :gen_tcp.close(socket)
-   end
-
-   def message(no, socket, {:unbindRequest, _}, db) do
-       :gen_tcp.close(socket)
+   def message(no, socket, {:bindRequest, {_,_,"cn=admin,dc=synrc,dc=com" = dn,{:simple, "secret"}}}, db) do
+       response = LDAP."BindResponse"(resultCode: :success, matchedDN: dn, diagnosticMessage: 'OK')
+       answer(response, no, :bindResponse, socket)
    end
 
    def message(no, socket, {:bindRequest, {_,_,bindDN,{:simple, password}}}, db) do
-       :io.format 'BIND DN: ~p~n', [bindDN]
        {:ok, statement} = Exqlite.Sqlite3.prepare(db,
-              "select rdn, att from ldap where val = ?1")
-       Exqlite.Sqlite3.bind(db, statement, [password])
+              "select rdn, att from ldap where rdn = ?1 and att = 'rootpw' and val = ?2")
+       Exqlite.Sqlite3.bind(db, statement, [bindDN,password])
        case Exqlite.Sqlite3.step(db, statement) do
-           :done -> 
-                     response = LDAP."BindResponse"(resultCode: :invalidCredentials,
-                     matchedDN: bindDN, diagnosticMessage: 'ERROR')
+           :done ->  code = :invalidCredentials
+#                     :io.format 'BIND Error: ~p~n', [code]
+                     response = LDAP."BindResponse"(resultCode: code,
+                         matchedDN: bindDN, diagnosticMessage: 'ERROR')
                      answer(response, no, :bindResponse, socket)
             {:row,[dn,password]} ->
+#                     :io.format 'BIND DN: ~p~n', [bindDN]
                      response = LDAP."BindResponse"(resultCode: :success,
-                     matchedDN: bindDN, diagnosticMessage: 'OK')
+                          matchedDN: bindDN, diagnosticMessage: 'OK')
                      answer(response, no, :bindResponse, socket)
        end
    end
 
    def message(no, socket, {:bindRequest, {_,_,bindDN,creds}}, db) do
        code = :authMethodNotSupported
-       :io.format 'BIND ERROR: ~p~n', [code]
+#       :io.format 'BIND ERROR: ~p~n', [code]
        response = LDAP."BindResponse"(resultCode: code,
           matchedDN: bindDN, diagnosticMessage: 'ERROR')
        answer(response, no, :bindResponse, socket)
    end
 
    def message(no, socket, {:searchRequest, {_,bindDN,scope,_,limit,_,_,filter,attributes}}, db) do
-       :io.format 'SEARCH DN: ~p~n', [bindDN]
-       :io.format 'SEARCH Scope: ~p~n', [scope]
-       :io.format 'SEARCH Filter: ~p~n', [filter]
-       :io.format 'SEARCH Attr: ~p~n', [attributes]
+#       :io.format 'SEARCH DN: ~p~n', [bindDN]
+#       :io.format 'SEARCH Scope: ~p~n', [scope]
+#       :io.format 'SEARCH Filter: ~p~n', [filter]
+#       :io.format 'SEARCH Attr: ~p~n', [attributes]
        resp = LDAP.'LDAPResult'(resultCode: :success, matchedDN: bindDN, diagnosticMessage: 'OK')
        :lists.map(fn [cn: commonName, email: email] ->
           cn = {:'PartialAttribute', "cn", [commonName]}
@@ -76,42 +83,75 @@ defmodule LDAP.TCP do
    end
 
    def message(no, socket, {:modDNRequest, {_,dn,rdn,old,_}}, db) do
-       :io.format 'MOD RDN DN: ~p~n', [dn]
-       :io.format 'MOD RDN newRDN: ~p~n', [rdn]
-       :io.format 'MOD RDN oldRDN: ~p~n', [old]
+#       :io.format 'MOD RDN DN: ~p~n', [dn]
+#       :io.format 'MOD RDN newRDN: ~p~n', [rdn]
+#       :io.format 'MOD RDN oldRDN: ~p~n', [old]
        resp = LDAP.'LDAPResult'(resultCode: :success, matchedDN: dn, diagnosticMessage: 'OK')
        answer(resp, no, :modDNResponse, socket)
    end
 
    def message(no, socket, {:modifyRequest, {_,dn, attributes}}, db) do
-       :io.format 'MOD DN: ~p~n', [dn]
-       :io.format 'MOD Attr: ~p~n', [attributes]
+#       :io.format 'MOD DN: ~p~n', [dn]
+#       :io.format 'MOD Attr: ~p~n', [attributes]
        resp = LDAP.'LDAPResult'(resultCode: :success, matchedDN: dn, diagnosticMessage: 'OK')
        answer(resp, no, :modifyResponse, socket)
    end
 
    def message(no, socket, {:compareRequest, {_,dn, assertion}}, db) do
-       :io.format 'CMP DN: ~p~n', [dn]
-       :io.format 'CMP Assertion: ~p~n', [assertion]
+#       :io.format 'CMP DN: ~p~n', [dn]
+#       :io.format 'CMP Assertion: ~p~n', [assertion]
        resp = LDAP.'LDAPResult'(resultCode: :success, matchedDN: dn, diagnosticMessage: 'OK')
        answer(resp, no, :compareResponse, socket)
    end
 
    def message(no, socket, {:addRequest, {_,dn, attributes}}, db) do
-       :io.format 'ADD DN: ~p~n', [dn]
-       :io.format 'ADD Attr: ~p~n', [attributes]
-       resp = LDAP.'LDAPResult'(resultCode: :success, matchedDN: dn, diagnosticMessage: 'OK')
-       answer(resp, no, :addResponse, socket)
+#       :io.format 'ADD REQ: ~p~n', [dn]
+      {:ok, statement} = Exqlite.Sqlite3.prepare(db, "select rdn, att, val from ldap where rdn = ?1")
+      Exqlite.Sqlite3.bind(db, statement, [dn])
+      case Exqlite.Sqlite3.step(db, statement) do
+            {:row, _} ->
+#                :io.format 'ADD ERROR: ~p~n', [dn]
+                resp = LDAP.'LDAPResult'(resultCode: :entryAlreadyExists, matchedDN: dn, diagnosticMessage: 'ERROR')
+                answer(resp, no, :addResponse, socket)
+            :done ->
+                :lists.map(fn {:PartialAttribute, att, vals} ->
+                    :lists.map(fn val ->
+                        {:ok, statement} = Exqlite.Sqlite3.prepare(db, "insert into ldap (rdn,att,val) values (?1,?2,?3)")
+                        :ok = Exqlite.Sqlite3.bind(db, statement, [dn,att,val])
+                        :done = Exqlite.Sqlite3.step(db, statement)
+                     end, vals)
+                end, attributes)
+#                :io.format 'ADD DN: ~p~n', [dn]
+#                :io.format 'ADD Attr: ~p~n', [attributes]
+                resp = LDAP.'LDAPResult'(resultCode: :success, matchedDN: dn, diagnosticMessage: 'OK')
+                answer(resp, no, :addResponse, socket)
+       end
    end
 
    def message(no, socket, {:delRequest, dn}, sql) do
-       :io.format 'DEL DN: ~p~n', [dn]
+#       :io.format 'DEL DN: ~p~n', [dn]
        resp = LDAP.'LDAPResult'(resultCode: :success, matchedDN: dn, diagnosticMessage: 'OK')
        answer(resp, no, :delResponse, socket)
    end
 
+   def message(no, socket, {:extendedReq,{_,oid,val}} = msg, db) do
+#       :io.format 'EXT: ~p~n', [msg]
+       res = LDAP.'ExtendedResponse'(resultCode: :unavailable, diagnosticMessage: 'ERROR',
+        responseName: oid, responseValue: val)
+       answer(res, no, :extendedResp, socket)
+   end
+
+   def message(no, socket, {:abandonRequest, _}, db), do: :gen_tcp.close(socket)
+   def message(no, socket, {:unbindRequest, _}, db), do: :gen_tcp.close(socket)
+
+   def message(no, socket, msg, sql) do
+       :io.format 'Invalid LDAP Message: ~p~n', [msg]
+       :gen_tcp.close(socket)
+   end
+
    def answer(response, no, op, socket) do
        message = LDAP."LDAPMessage"(messageID: no, protocolOp: {op, response})
+#       :io.format '~p~n', [message]
        {:ok, bytes} = :'LDAP'.encode(:'LDAPMessage', message)
        :gen_tcp.send(socket, :erlang.iolist_to_binary(bytes))
    end
@@ -122,7 +162,10 @@ defmodule LDAP.TCP do
                  case :'LDAP'.decode(:'LDAPMessage',data) do
                       {:ok,decoded} ->
                           {:'LDAPMessage', no, payload, _} = decoded
-                          spawn(fn -> message(no, socket, payload, db) end)
+                          :erlang.spawn(fn ->
+                              res = :timer.tc(fn -> message(no, socket, payload, db) end)
+#                              :io.format 'Time: ~p~n', [res]
+                          end)
                           loop(socket, db)
                       {:error,_} -> :exit
                  end
--- lib/ldap_test.ex
+++ lib/ldap_test.ex
@@ -3,7 +3,7 @@ defmodule LDAP.Test do
 
   def testConnection() do
       {:ok, conn} = :eldap.open(['127.0.0.1'], [{:port, 636}, {:ssl, true}])
-      :eldap.start_tls(conn, [])
+#      :eldap.start_tls(conn, [])
       :ok = :eldap.simple_bind(conn, 'cn=admin,dc=synrc,dc=com', 'secret')
       filter = :eldap.and([ :eldap.substrings('cn',[{:any, '100000'}]) ])
       scope = :wholeSubtree
@@ -16,23 +16,29 @@ defmodule LDAP.Test do
   end
 
   def loop1M() do
-      :lists.map(fn i -> create(i) end, :lists.seq(1,1_000_000))
+      :lists.map(fn i -> create(i) end, :lists.seq(1,500))
   end
 
   def create(no0) do
-      {:ok, conn} = :eldap.open(['127.0.0.1'], [{:port, 636}, {:ssl, true}])
-      :eldap.start_tls(conn, [])
+#      LDAP.TCP.start
+#      :timer.sleep(500)
+      {:ok, conn} = :eldap.open(['127.0.0.1'], [{:port, 1489}])
+#      {:ok, conn} = :eldap.open(['127.0.0.1'], [{:port, 636}, {:ssl, true}])
+#      :eldap.start_tls(conn, [])
       :eldap.simple_bind(conn, 'cn=admin,dc=synrc,dc=com', 'secret')
+      case rem(no0,100) do
+           0 -> :io.format 'Client bind: ~p~n', [no0]
+           _ -> :ok end
       no = case no0 do x when is_integer(x) -> :erlang.integer_to_list(x) ; _ -> :io_lib.format('~s',[no0]) end
       name = 'user_' ++ no
-      res = :eldap.add(conn, 'cn='++name++', ou=People, dc=synrc, dc=com',
+      {time,res} = :timer.tc(fn -> :eldap.add(conn, 'cn='++name++', ou=People, dc=synrc, dc=com',
        [{ 'objectClass', ['person','inetorgperson']},
         { 'givenName', ['givenName ' ++ no]},
         { 'cn', [name]},
         { 'sn', ['surname ' ++ no]},
         { 'telephoneNumber', ['+380670001122']}]
-      )
-      :io.format 'Create: ~p~n', [res]
+      ) end)
+#      :io.format 'Time: ~p~n', [time]
       :eldap.close(conn)
   end
 
